b38295133f3d79db130fdd6d1bb4bcc502f57ee0
e-matteson <e.r.matteson@gmail.com>
2018-10-12 17:16:45 -0400

    feat: set usb product description to "USBEventMarker"
    For easily figuring out which device is the marker

---
+++ product-name.c
@@ -0,0 +1,17 @@
+#include "usb_names.h"
+
+// Override the product name, so the computer can tell that this device is the
+// USB Event Marker, and knows to send event codes to it (instead of
+// accidentally sending them to some other unsuspecting device...)
+//
+// See discussion here: https://forum.pjrc.com/threads/23796-How-to-change-Teensy-3-0-PRODUCT_NAME
+
+#define PRODUCT_NAME { 'U', 'S', 'B', 'E', 'v', 'e', 'n', 't', 'M', 'a', 'r', 'k', 'e', 'r' }
+#define PRODUCT_NAME_LEN 14
+
+
+struct usb_string_descriptor_struct usb_string_product_name = {
+    2 + PRODUCT_NAME_LEN * 2,  // uint8_t bLength
+    3,                         // uint8_t bDescriptorType
+    PRODUCT_NAME               // uint16_t wString[]
+};
